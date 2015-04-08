e58abacc411c25f9eeedafe90e9b561e2cd1e279
weitaoyang <370151408@qq.com>
2015-04-08 16:05:43 +0800

    添加一个Parcel

---
+++ testhttp/src/com/example/testhttp/Book.java
@@ -0,0 +1,80 @@
+package com.example.testhttp;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Book implements Parcelable
+{
+	private String bookName;
+    private String author;
+    private int publishDate;
+    
+	public String getBookName()
+	{
+		return bookName;
+	}
+
+	public void setBookName(String bookName)
+	{
+		this.bookName = bookName;
+	}
+
+	public String getAuthor()
+	{
+		return author;
+	}
+
+	public void setAuthor(String author)
+	{
+		this.author = author;
+	}
+
+	public int getPublishDate()
+	{
+		return publishDate;
+	}
+
+	public void setPublishDate(int publishDate)
+	{
+		this.publishDate = publishDate;
+	}
+
+	
+	
+	@Override
+	public int describeContents()
+	{
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags)
+	{
+		dest.writeString(bookName);
+		dest.writeString(author);
+		dest.writeInt(publishDate);
+	}
+	
+	public static final Parcelable.Creator<Book> CREATOR = new Creator<Book>()
+    {
+        @Override
+        public Book[] newArray(int size)
+        {
+            return new Book[size];
+        }
+        
+        @Override
+        public Book createFromParcel(Parcel in)
+        {
+            return new Book(in);
+        }
+    };
+    
+	public Book(Parcel in)
+    {
+        bookName = in.readString();
+        author = in.readString();
+        publishDate = in.readInt();
+    }
+
+}
